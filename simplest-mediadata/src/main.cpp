1957b56b1422abe7ce0d8420247cb3d443df899c
zhoulee <295861542@qq.com>
2020-12-10 15:57:49 +0800

    配置工程

--- simplest-mediadata/src/main.cpp
+++ simplest-mediadata/src/main.cpp
@@ -1,10 +1,9 @@
 #include "SimplestMediadata.h"
-
 #include <iostream>
 
 const std::string resourcePath("E:\\github\\streaming-media\\resource\\");
 
-int main()
+void pcm()
 {
     //const std::string pcm16lePath = resourcePath + std::string("NocturneNo2inEflat_44.1k_s16le.pcm");
     //simplest_pcm16le_split(pcm16lePath);
@@ -14,10 +13,19 @@ int main()
     //simplest_pcm16le_to_pcm8(pcm16lePath);
 
     //simplest_pcm16le_to_wave(pcm16lePath, 2, 44100);
+}
 
+void aac()
+{
     simplest_aac_parser(resourcePath + std::string("nocturne.aac"));
+}
 
+void flv()
+{
     simplest_flv_parser(resourcePath + std::string("cuc_ieschool.flv"));
+}
 
+int main()
+{
     return 0;
 }
